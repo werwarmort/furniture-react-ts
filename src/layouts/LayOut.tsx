67712c6fd03104f7e7b3e09265e0d2714f6503ed
werwarmort <70599393+werwarmort@users.noreply.github.com>
2023-08-10 16:19:02 +0300

    commit pleasegit add ../src/layouts/LayOut.tsx ../tsconfig.json!

--- src/layouts/LayOut.tsx
+++ src/layouts/LayOut.tsx
@@ -12,12 +12,6 @@ const Layout = () => {
 
   return (
     <>
-      {/* <header>
-        <CustomLink to="/">Home</CustomLink>
-        <CustomLink to="/posts">Blog</CustomLink>
-        <CustomLink to="about">About</CustomLink>
-      </header> */}
-
       <div className="wrapper">
         <header className="header">
           <div className="container">
