e596f6773d013c964bf38218d9b69f65b1ae02f1
werwarmort <70599393+werwarmort@users.noreply.github.com>
2023-08-24 12:17:58 +0300

    comments is ready

--- server/generateComments.js
+++ server/generateComments.js
@@ -1,22 +1,28 @@
 const fs = require("fs");
 const faker = require("faker");
+const moment = require("moment");
 
 const NUM_COMMENTS = 400;
 
-const generateComment = (postId) => {
-  return {
-    postId,
-    name: faker.Name.findName(),
-    email: faker.Internet.email(), // Добавили генерацию email
-    date: faker.Date.between(2015, 2023),
-    message: faker.Lorem.words(Math.floor(Math.random() * 35) + 5).join(" "), // Используем .join(' ') для объединения слов в текст
-    photo: faker.Image.avatar(),
-    replies: [],
-  };
-};
-
 const generateComments = () => {
   const comments = [];
+  let commentId = 1;
+
+  const generateComment = (postId) => {
+    const date = faker.Date.between("2015-01-01", "2023-12-31");
+    const comment = {
+      postId,
+      commentId,
+      name: faker.Name.findName(),
+      email: faker.Internet.email(),
+      date: moment(date).format("MMMM D, YYYY"),
+      message: faker.Lorem.words(Math.floor(Math.random() * 35) + 5).join(" "),
+      photo: faker.Image.avatar(),
+      replies: [],
+    };
+    commentId++;
+    return comment;
+  };
 
   for (let postId = 1; postId <= NUM_COMMENTS / 4; postId++) {
     const numComments = Math.floor(Math.random() * 5) + 1;
--- server/server.js
+++ server/server.js
@@ -27,14 +27,50 @@ app.get("/comments", (req, res) => {
       return;
     }
     try {
+      // const data = fs.readFile("./server/comments.json", "utf8");
       const comments = JSON.parse(data);
-      res.json(comments);
+      // console.log("полученные комменты на сервере: ", comments);
+
+      const { postId } = req.query;
+      console.log("постID пришедший на сервер", postId);
+
+      if (postId) {
+        const filteredComments = comments.filter(
+          (comment) => comment.postId == postId
+        );
+
+        res.json(filteredComments);
+        console.log("отфильтрованные комменты: ", filteredComments);
+      } else {
+        res.json(comments);
+      }
     } catch (parseError) {
       console.error("Ошибка парсинга JSON:", parseError);
       res.status(500).json({ error: "Ошибка при обработке данных" });
     }
   });
 });
+
+// app.get("/comments", async (req, res) => {
+//   try {
+//     const data = fs.readFile("./server/comments.json", "utf8");
+//     const comments = JSON.parse(data);
+
+//     const { postId } = req.query;
+
+//     if (postId) {
+//       const filteredComments = comments.filter(
+//         (comment) => comment.postId === postId
+//       );
+//       res.json(filteredComments);
+//     } else {
+//       res.json(comments);
+//     }
+//   } catch (error) {
+//     console.error("Ошибка:", error);
+//     res.status(500).json({ error: "Произошла ошибка" });
+//   }
+// });
 
 app.get("/posts", (req, res) => {
   fs.readFile("./server/posts.json", "utf8", (err, data) => {
@@ -52,6 +88,25 @@ app.get("/posts", (req, res) => {
     }
   });
 });
+
+// const comments = require('./comments.json'); // Загрузите данные комментариев
+
+// app.get("/comments", (req, res) => {
+//   fs.readFile("./server/comments.json", "utf8", (err, data) => {
+//     if (err) {
+//       console.error("Ошибка чтения файла:", err);
+//       res.status(500).json({ error: "Произошла ошибка" });
+//       return;
+//     }
+//     try {
+//       const comments = JSON.parse(data);
+//       res.json(comments);
+//     } catch (parseError) {
+//       console.error("Ошибка парсинга JSON:", parseError);
+//       res.status(500).json({ error: "Ошибка при обработке данных" });
+//     }
+//   });
+// });
 
 app.listen(port, () => {
   console.log(`Сервер запущен на порту ${port}`);
